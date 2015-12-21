f650527d5f4bf9a2e35bd9fe06038d619bad2bf5
Ambros94 <luca_ambro94@hotmail.it>
2015-12-21 17:51:04 +0100

    fgfg

---
+++ app/MongoManager.js
@@ -0,0 +1,29 @@
+/* MongoManager.js
+*
+*  Handles Database Connection
+*
+* */
+
+
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = require('../config/database').url;
+
+module.exports = new Promise(function (resolve, reject) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            reject(err);
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            console.log('Connection established to', url);
+            resolve(db);
+        }
+    });
+});
+
+
+
