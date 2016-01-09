7247443f5b3718e50114f80d339bd656a10be8d8
marziostampanoni <marzio.stampanoni@student.supsi.ch>
2016-01-09 11:18:34 +0100

    view improvement

--- app/routes.js
+++ app/routes.js
@@ -153,7 +153,7 @@ module.exports = function (app, passport) {
                     });
                 });
         }
-        
+
         else {
             res.redirect('/login/terms');
         }
